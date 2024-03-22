886ea66461031c3a3839c3f685990f8abf2f9e8e
Damian Galeano <dj.damian.b@hotmail.com>
2024-03-22 23:29:00 +0100

    Create Message

--- part1/mensaje.js
+++
@@ -1,6 +0,0 @@
-
-const Mensaje = (params) =>  {
-    return <h1>Hola funciona desde el modulo</h1>
-}
-
-export default Mensaje
--- part1/src/App.js
+++ part1/src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-import './mensaje.js';
+ import './App.css';
 
+const Mensaje = () => {
+  return <h1>Hola mundo</h1>
+}
+
+const Description = () => {
+  return <p>Esta es la app del curso fullStack</p>
+}
 
 const  App = () => {
-
   return (
     <div className="App">
-     <Mensaje message='Trabajando en React'/>
+     <Mensaje/>
+     <Mensaje/>
+     <Mensaje/>
+
+     <Description/>
     </div>
   );
 }
 
-export default App;
+export default App; 
